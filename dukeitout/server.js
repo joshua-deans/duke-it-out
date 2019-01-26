f1fe9a9f5a268fb7c1fe3891cec6f96c4b7d735a
Joshua Deans <joshua.deans21@gmail.com>
2019-01-26 14:48:25 -0800

    node_module files deleted

--- dukeitout/server.js
+++ dukeitout/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
 app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
