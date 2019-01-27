c576eca84982aba9765557b3fb2ebca1b5afd350
Joshua Deans <joshua.deans21@gmail.com>
2019-01-26 16:16:49 -0800

    started mysql statement

--- dukeitout/server.js
+++ dukeitout/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mysql = require('mysql');
 const port = process.env.PORT || 5000;
+const connection =  mysql.createConnection(
+    {host: "duke-it-out-mysqldbserver.mysql.database.azure.com",
+      user: "mysqldbuser@duke-it-out-mysqldbserver",
+      password: "nwHacks2019",
+      database: {your_database},
+      port: 3306});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,12 +28,12 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
