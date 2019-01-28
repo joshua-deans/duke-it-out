7a3b5bcb7cb3abc7ce4c3b8c729030c245bf43d8
Joshua Deans <joshua.deans21@gmail.com>
2019-01-27 21:49:43 -0800

    Updated node version

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -20,6 +21,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
+
+if (process.env.NODE_ENV === 'production') {
+// Set static folder
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // io.on('connection', function(socket){
 //   console.log('a user connected');
