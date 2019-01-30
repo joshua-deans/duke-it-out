9cc8320b5bc055a8039ba5214a8ba822436d570b
Ethan Li <ethan.li@move.com>
2019-01-30 00:50:13 -0800

    Add routing and changing components with route

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import CreateRoom from './CreateRoom';
 
 class App extends Component {
   state = {
@@ -8,7 +10,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <Link to="/">Home</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/Create">Create</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <div>
+          <Route path="/Create" component={CreateRoom}/>
+        </div>
       </div>
     );
   }
