23cfdd3b0ad82d673dd895944500f1cc9cd70aa7
Joshua Deans <joshua.deans21@gmail.com>
2019-02-01 21:21:31 -0800

    Modified API functions to be more maintainable

---
+++ config.js
@@ -0,0 +1,7 @@
+config = {host: "duke-it-out-mysql.mysql.database.azure.com",
+        user: "mysqldbadmin@duke-it-out-mysql",
+        password: "nwHacks2019",
+        database: "dukeitout",
+        port: 3306};
+
+module.exports = config;
