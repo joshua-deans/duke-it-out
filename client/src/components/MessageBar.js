1c6263089a8f23e7cb1286673cfbf7f096077a75
Joshua Deans <joshua.deans21@gmail.com>
2019-02-05 20:47:41 -0800

    Made improvements to the CSS of the application

--- client/src/components/MessageBar.js
+++ client/src/components/MessageBar.js
@@ -11,7 +11,7 @@ const MessageBar = () => {
         <input className="form-control" style={msgBarStyle} type="text" placeholder="Write a message"
                aria-label="Write a message" aria-describedby="send-msg-btn"/>
         <div className="input-group-append">
-          <button className="btn btn-outline-secondary btn-info" type="button" id="send-msg-btn">Send</button>
+          <button className="btn btn-primary" type="button" id="send-msg-btn">Send</button>
         </div>
       </div>
   )
