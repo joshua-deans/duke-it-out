84d027852ff4a5e1e1e79c9efe3f8b54866462a2
Joshua Deans <joshua.deans21@gmail.com>
2019-02-14 19:42:18 -0800

    Fixed React build

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
 // Set static folder
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
